de572c4a32fb1188e576e66f0616bb109c53f67b
Wonderest <50113417+wonderest@users.noreply.github.com>
2019-07-19 19:11:54 +0700

    Add files via upload

---
+++ starcell/app/controllers/Voucher.php
@@ -0,0 +1,9 @@
+<?php
+
+class Voucher extends Controller
+{
+    public function index()
+    {
+        $this->view('voucher/index');
+    }
+}
---
+++ starcell/app/views/voucher/index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h1>Voucher</h1>
+    </header>
+    <table>
+        <tr>
+            <th>Provider</th>
+            <th>Kuota</th>
+            <th>Harga</th>
+            <th>Stok</th>
+        </tr>
+        <tr>
+            <td class="prov" rowspan="4">XL</td>
+            <td>4.5 Gb</td>
+            <td>33.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>8 Gb</td>
+            <td>45.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>15 Gb</td>
+            <td>70.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>21 Gb</td>
+            <td>110.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td class="prov" rowspan="4">Axis</td>
+            <td>1 Gb</td>
+            <td>18.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>2 Gb</td>
+            <td>28.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>3 Gb</td>
+            <td>38.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>5 Gb</td>
+            <td>55.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td class="prov" rowspan="4">Smartfren</td>
+            <td>10 Gb</td>
+            <td>30.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>16 Gb</td>
+            <td>45.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>30 Gb</td>
+            <td>65.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>Unlimited</td>
+            <td>70.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td class="prov" rowspan="4">IM3</td>
+            <td>1 Gb</td>
+            <td>22.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>8 Gb</td>
+            <td>42.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>3 Gb</br>Unlimited</td>
+            <td>53.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>7 Gb</br>Unlimited</td>
+            <td>75.000</td>
+            <td>10</td>
+        </tr>
+    </table>
+    <header>
+        <h1>Perdana Internet</h1>
+    </header>
+    <table>
+        <tr>
+            <th>Provider</th>
+            <th>Kuota</th>
+            <th>Harga</th>
+            <th>Stok</th>
+        </tr>
+        <tr>
+            <td class="prov" rowspan="4">XL</td>
+            <td>4.5 Gb</td>
+            <td>33.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>8 Gb</td>
+            <td>45.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>15 Gb</td>
+            <td>70.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>21 Gb</td>
+            <td>110.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td class="prov" rowspan="4">Axis</td>
+            <td>1 Gb</td>
+            <td>15.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>2 Gb</td>
+            <td>30.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>3 Gb</td>
+            <td>40.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>5 Gb</td>
+            <td>55.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td class="prov" rowspan="5">Smartfren</td>
+            <td>5 Gb</td>
+            <td>17.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>10 Gb</td>
+            <td>32.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>16 Gb</td>
+            <td>45.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>30 Gb</td>
+            <td>68.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>Unlimited</td>
+            <td>70.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td class="prov" rowspan="4">IM3</td>
+            <td>3.5 Gb</td>
+            <td>20.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>2 Gb</br>Unlimited</td>
+            <td>38.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>3 Gb</br>Unlimited</td>
+            <td>53.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td>7 Gb</br>Unlimited</td>
+            <td>70.000</td>
+            <td>10</td>
+        </tr>
+        <tr>
+            <td class="prov" rowspan="6">3</br>Three</td>
+            <td>1 Gb</td>
+            <td>10.000</td>
+            <td>Ada</td>
+        </tr>
+        <tr>
+            <td>50 Gb</td>
+            <td>85.000</td>
+            <td>Ada</td>
+        </tr>
+        <tr>
+            <td>2 Gb</td>
+            <td>23.000</td>
+            <td>Ada</td>
+        </tr>
+        <tr>
+            <td>9 Gb</td>
+            <td>52.000</td>
+            <td>Ada</td>
+        </tr>
+        <tr>
+            <td>18 Gb</td>
+            <td>85.000</td>
+            <td>Ada</td>
+        </tr>
+        <tr>
+            <td>30 Gb</td>
+            <td>140.000</td>
+            <td>Ada</td>
+        </tr>
+    </table>
+</body>
+
+</html>
